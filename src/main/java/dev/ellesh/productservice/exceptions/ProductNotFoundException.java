28ddfbae3bfe2fc11e77165692441f69a044ed1d
Ellesh <kelothellesh@gmail.com>
2026-06-06 13:39:41 +0530

    exception classes

---
+++ src/main/java/dev/ellesh/productservice/exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.ellesh.productservice.exceptions;
+
+public class ProductNotFoundException extends Exception {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
