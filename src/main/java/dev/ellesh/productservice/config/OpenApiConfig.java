429b7d13f4060d1f15d545217b2f901bf13c5849
Ellesh <kelothellesh@gmail.com>
2026-06-06 13:57:02 +0530

    swagger doc

---
+++ src/main/java/dev/ellesh/productservice/config/OpenApiConfig.java
@@ -0,0 +1,15 @@
+package dev.ellesh.productservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.core.jackson.ModelResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public ModelResolver modelResolver(ObjectMapper objectMapper) {
+        return new ModelResolver(objectMapper);
+    }
+}
