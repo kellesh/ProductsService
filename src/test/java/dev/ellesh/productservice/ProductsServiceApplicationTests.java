1a4e1abc0e2d5024b8fead0a6bc8c2e07c62ba20
Ellesh <kelothellesh@gmail.com>
2026-06-06 13:40:55 +0530

    test class

---
+++ src/test/java/dev/ellesh/productservice/ProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ellesh.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
